0fb489b996945e416988ae611f1821f5b6bb2268
Seebles <seebles72@gmail.com>
2024-01-17 15:42:11 -0700

    Started creating routes

---
+++ tickets.py
@@ -0,0 +1,16 @@
+from Route import Route
+
+routes = [
+	Route("Boston", "Miami", 12),
+	Route("Calgary", "Phoenix", 13),
+	Route("Calgary", "Salt Lake City", 7),
+	Route("Chicago", "New Orleans", 7),
+	Route("Chicago", "Santa Fe", 9),
+	Route("Dallas", "New York", 11),
+	Route("Denver", "El Paso", 4),
+	Route("Denver", "Pittsburgh", 11),
+	Route("Duluth", "El Paso", 10),
+	Route("Duluth", "Houston", 8),
+	Route("Helena", "Los Angeles", 8),
+
+]

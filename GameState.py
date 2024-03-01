2f7ad6cf73b7c909e311f096b632964119834b71
752986 <112513827+752986@users.noreply.github.com>
2024-03-01 09:22:49 -0700

    Set up GameState fields

--- GameState.py
+++ GameState.py
@@ -1,3 +1,14 @@
+from Board import Board
+from TrainColor import TrainColor
+from Ticket import Ticket
+from Player import Player
+
 class GameState:
 	# board state, players, draw piles, APIs for player actions, 
-	pass
+	board: Board # the game board
+	draw_pile: list[TrainColor] # the draw pile for train cards
+	visible_pile: list[TrainColor] # the 5 face-up cards
+	ticket_pile: list[Ticket] # the draw pile for destination ticket cards
+	players: list[Player] # the players in the game
+	current_turn: int # the current player number
+	

b3a200329926dd306188bf9ffea4a57713600893
752986 <112513827+752986@users.noreply.github.com>
2024-03-07 10:23:14 -0700

    Add name field to Player

--- Player.py
+++ Player.py
@@ -2,15 +2,18 @@ from Ticket import Ticket
 from TrainColor import TrainColor
 
 class Player:
+	name: str
 	score: int
 	hand: list[int] # each index corresponds to one train color; it stores the amount of that color
 	trains: int # the number of plastic trains left
 	tickets: list[Ticket] # the ticket cards currently held
 
-	def __init__(self):
+	def __init__(self, name: str):
+		self.name = name
 		self.score = 0
 		self.hand = [0 for _ in TrainColor]
 		self.trains = 45
+		self.tickets = []
 
 	def add_cards(self, color: TrainColor, amount: int = 1):
 		self.hand[color.value] += amount

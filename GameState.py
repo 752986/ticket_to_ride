6083920f4112a49d58b8fb7a03cdb2162213b6ba
752986 <112513827+752986@users.noreply.github.com>
2024-03-07 10:54:45 -0700

    Implement GameState draw methods

--- GameState.py
+++ GameState.py
@@ -6,6 +6,10 @@ import random
 
 class GameState:
 	# board state, players, draw piles, APIs for player actions, 
+	
+	class InvalidAction(Exception):
+		'''A player action was attempted that the player cannot do.'''
+	
 	board: Board # the game board
 	draw_pile: list[TrainColor] # the draw pile for train cards
 	visible_pile: list[TrainColor] # the 5 face-up cards
@@ -15,7 +19,8 @@ class GameState:
 	current_turn: int # the current player number
 
 	def _draw(self, amount: int = 1) -> list[TrainColor]:
-		'''Draw the specified number of cards from the draw pile, reshuffling the discard pile if needed.'''
+		'''Draws the specified number of cards from the draw pile, reshuffling the discard pile if needed.'''
+
 		result: list[TrainColor] = []
 		for _ in range(amount):
 			if len(self.draw_pile) == 0:
@@ -24,20 +29,48 @@ class GameState:
 				random.shuffle(self.draw_pile)
 			result.insert(0, self.draw_pile.pop()) # inserting rather than appending to preserve card order, idk if this is needed
 
+		return result
+
 	def _discard(self, color: TrainColor, amount: int = 1):
+		'''Adds the specified number of cards to the discard pile.
+		If discarding multiple colors, this method will need to be called multiple times.
+		'''
+
 		self.discard_pile.extend((color for _ in range(amount)))
+	
+	def current_player(self) -> Player:
+		return self.players[self.current_turn]
 
 	def claim(self, start: str, end: str, color: TrainColor):
 		'''Claims the specified route for the current player, spending the correct amount of resources.'''
 	
 	def draw_blind(self, amount: int = 1):
 		'''Draws the specified number of cards from the draw pile into the current player's hand.'''
+		cards = self._draw(amount)
+		for card in cards:
+			self.current_player().add_cards(card)
 	
 	def draw_visible(self, color: TrainColor):
-		'''Draws the specified card from the face-up pile into the current player's hand.'''
+		'''Draws the specified card from the face-up pile into the current player's hand, replacing it.'''
+
+		if color not in self.visible_pile:
+			raise GameState.InvalidAction(f"{color} is not present in the face-up draw pile.")
+		
+		self.visible_pile.remove(color)
+		self.visible_pile.append(self._draw()[0])
+		self.current_player().add_cards(color)
 
 	def draw_tickets(self, amount: int) -> list[Ticket]:
 		'''Draws the specified number of tickets from the ticket draw pile and returns them.
 		Note that unlike the other draw methods, this doesn't add them to the current player.
 		This is done so that the player may choose which ones to keep.
 		'''
+
+		if len(self.ticket_pile) < amount:
+			raise GameState.InvalidAction(f"There are not enough tickets in the pile ({amount} requested, {len(self.ticket_pile)} available).")
+		
+		result: list[Ticket] = []
+		for _ in range(amount):
+			result.append(self.ticket_pile.pop())
+
+		return result
